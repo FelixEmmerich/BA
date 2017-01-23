7b58bc99f74d53dbe2258b65f4a1071c9b208b6a
FelixEmmerich <Felix-Emmerich@Freenet.de>
2017-01-23 15:41:18 +0100

    Added EnvironmentRequirements class.

---
+++ WEKITPrototypeHL/Assets/Game Mechanism Framework/Scripts/EnvironmentRequirements.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+
+namespace GameMechanism
+{
+    public class EnvironmentRequirements : MonoBehaviour
+    {
+
+        //Types coresponding to and comments taken from PlaySpaceStats (from HoloToolkit's SpatialUnderstandingDLL)
+        public enum RequirementCategory
+        {
+            HorizSurfaceArea, // In m2 : All horizontal faces UP between Ground – 0.15 and Ground + 1.f (include Ground and convenient horiz surface)
+            TotalSurfaceArea, // In m2 : All !
+            UpSurfaceArea, // In m2 : All horizontal faces UP (no constraint => including ground)
+            DownSurfaceArea, // In m2 : All horizontal faces DOWN (no constraint => including ceiling)
+            WallSurfaceArea, // In m2 : All Vertical faces (not only walls)
+            VirtualCeilingSurfaceArea, // In m2 : estimation of surface of virtual Ceiling.
+            VirtualWallSurfaceArea, // In m2 : estimation of surface of virtual Walls.
+            NumFloor, // List of Area of each Floor surface (contains count)
+            NumCeiling, // List of Area of each Ceiling surface (contains count)
+            NumWall_XNeg, // List of Area of each Wall XNeg surface (contains count)
+            NumWall_XPos, // List of Area of each Wall XPos surface (contains count)
+            NumWall_ZNeg, // List of Area of each Wall ZNeg surface (contains count)
+            NumWall_ZPos, // List of Area of each Wall ZPos surface (contains count)
+            NumWall, // Number of walls total (XNeg, XPos, ZNeg, ZPos)
+            NumPlatform // List of Area of each Horizontal not Floor surface (contains count)
+        }
+
+        public struct Requirement
+        {
+            public RequirementCategory Category;
+            public float Amount;
+            [Tooltip("How required amount is compared to actual amount. If false, a value lower than Amount is required.")]
+            public bool GreaterThanOrEqual;
+        }
+
+        public Requirement[] Requirements;
+        // Use this for initialization
+
+        public bool CheckRequirements()
+        {
+            for (int i=Requirements.Length-1;i>=0;i--)
+            {
+                //Return false if required amount does not behave to actual amount as specified
+                if (Requirements[i].Amount>=GetStatsValueFromCategory(Requirements[i].Category)==Requirements[i].GreaterThanOrEqual)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool CheckAllRequirements(out bool[] status)
+        {
+            bool result = true;
+            status = new bool[Requirements.Length];
+            for (int i = Requirements.Length - 1; i >= 0; i--)
+            {
+                //Return false if required amount does not behave to actual amount as specified
+                status[i] = (Requirements[i].Amount >= GetStatsValueFromCategory(Requirements[i].Category) ==
+                             Requirements[i].GreaterThanOrEqual);
+                if (!status[i])
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+
+        private float GetStatsValueFromCategory(RequirementCategory category)
+        {
+            SpatialUnderstandingDll.Imports.PlayspaceStats stats = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStats();
+            switch (category)
+            {
+                case RequirementCategory.HorizSurfaceArea:
+                    return stats.HorizSurfaceArea;
+                case RequirementCategory.TotalSurfaceArea:
+                    return stats.TotalSurfaceArea;
+                case RequirementCategory.UpSurfaceArea:
+                    return stats.UpSurfaceArea;
+                case RequirementCategory.DownSurfaceArea:
+                    return stats.DownSurfaceArea;
+                case RequirementCategory.WallSurfaceArea:
+                    return stats.WallSurfaceArea;
+                case RequirementCategory.VirtualCeilingSurfaceArea:
+                    return stats.VirtualCeilingSurfaceArea;
+                case RequirementCategory.VirtualWallSurfaceArea:
+                    return stats.VirtualWallSurfaceArea;
+                case RequirementCategory.NumFloor:
+                    return stats.NumFloor;
+                case RequirementCategory.NumCeiling:
+                    return stats.NumCeiling;
+                case RequirementCategory.NumWall_XNeg:
+                    return stats.NumWall_XNeg;
+                case RequirementCategory.NumWall_XPos:
+                    return stats.NumWall_XPos;
+                case RequirementCategory.NumWall_ZNeg:
+                    return stats.NumWall_ZNeg;
+                case RequirementCategory.NumWall_ZPos:
+                    return stats.NumWall_ZPos;
+                case RequirementCategory.NumWall:
+                    return stats.NumWall_XPos + stats.NumWall_XNeg + stats.NumWall_ZNeg + stats.NumWall_ZPos;
+                case RequirementCategory.NumPlatform:
+                    return stats.NumPlatform;
+                default:
+                    Debug.Log("Category not defined");
+                    return -1;
+            }
+        }
+
+    }
+}
--- WEKITPrototypeHL/Assets/Game Mechanism Framework/Scripts/ScanStateGUI.cs
+++ WEKITPrototypeHL/Assets/Game Mechanism Framework/Scripts/ScanStateGUI.cs
@@ -5,51 +5,55 @@ using HoloToolkit.Unity;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScanStateGUI : MonoBehaviour
+namespace GameMechanism
 {
-    public Text ScanStateText;
-    public Text StatsText;
-    private bool _scanning;
-    private int _frameCount;
-    private IntPtr _statsPtr;
-
-    // Use this for initialization
-    void Start()
+    public class ScanStateGUI : MonoBehaviour
     {
-        _scanning = true;
-        SpatialUnderstanding.Instance.ScanStateChanged += OnStateChange;
-    }
+        public Text ScanStateText;
+        public Text StatsText;
+        private bool _scanning;
+        private int _frameCount;
+        private IntPtr _statsPtr;
 
-    void OnStateChange()
-    {
-        /*if (!_scanning && (int) SpatialUnderstanding.Instance.ScanState > 0)
+        // Use this for initialization
+        void Start()
         {
             _scanning = true;
-        }*/
-        ScanStateText.text = "Scan state: " + SpatialUnderstanding.Instance.ScanState;
+            SpatialUnderstanding.Instance.ScanStateChanged += OnStateChange;
+        }
+
+        void OnStateChange()
+        {
+            /*if (!_scanning && (int) SpatialUnderstanding.Instance.ScanState > 0)
+            {
+                _scanning = true;
+            }*/
+            ScanStateText.text = "Scan state: " + SpatialUnderstanding.Instance.ScanState;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_scanning)
+            {
+                CheckForStats();
+            }
+        }
+
+        private void CheckForStats()
+        {
+            if (_statsPtr == IntPtr.Zero)
+            {
+                _statsPtr = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStatsPtr();
+            }
+            if (((++_frameCount % 10) == 0) && (SpatialUnderstandingDll.Imports.QueryPlayspaceStats(_statsPtr) != 0))
+            {
+                var stats = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStats();
+                StatsText.text =
+                (String.Format("total {0} wall {1} horizontal {2} ceiling {3}", stats.TotalSurfaceArea,
+                    stats.WallSurfaceArea, stats.HorizSurfaceArea, stats.VirtualCeilingSurfaceArea));
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (_scanning)
-        {
-            CheckForStats();
-        }
-    }
-
-    private void CheckForStats()
-    {
-        if (_statsPtr == IntPtr.Zero)
-        {
-            _statsPtr = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStatsPtr();
-        }
-        if (((++_frameCount % 10) == 0) && (SpatialUnderstandingDll.Imports.QueryPlayspaceStats(_statsPtr) != 0))
-        {
-            var stats = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStats();
-            StatsText.text =
-            (String.Format("total {0} wall {1} horizontal {2} ceiling {3}", stats.TotalSurfaceArea,
-                stats.WallSurfaceArea, stats.HorizSurfaceArea, stats.VirtualCeilingSurfaceArea));
-        }
-    }
 }
