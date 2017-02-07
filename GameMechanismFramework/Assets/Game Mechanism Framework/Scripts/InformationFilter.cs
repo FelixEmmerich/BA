8a558f20ecac4d7861afa30f9bf1ba1f7b5148e5
FelixEmmerich <Felix-Emmerich@Freenet.de>
2017-02-07 17:52:07 +0100

    InformationFilter generally works now (distance metric not created yet), probably performs awfully though.

--- GameMechanismFramework/Assets/Game Mechanism Framework/Scripts/InformationFilter.cs
+++ GameMechanismFramework/Assets/Game Mechanism Framework/Scripts/InformationFilter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -27,6 +26,7 @@ namespace GameMechanism
             public bool _previouslytrue;
         }
 
+        [Tooltip("UnityEvents are called in order, so sort RuleSets in ascending order. e.g. If multiple rules change one UI text, the last one in this array will be executed last.")]
         public RuleSet[] RuleSets;
 
         // Update is called once per frame
@@ -37,28 +37,75 @@ namespace GameMechanism
 
         public void CheckRuleSets()
         {
-            List<InformationFilterMetric> _changedMetrics = new List<InformationFilterMetric>();
+            List<InformationFilterMetric> changedMetrics = new List<InformationFilterMetric>();
             for (int i = 0; i < RuleSets.Length; i++)
             {
-                bool conditionsMet = true;
+                bool allConditionsMet = true;
+                List<InformationFilterMetric> trueMetrics = new List<InformationFilterMetric>();
+                List<InformationFilterMetric> falseMetrics = new List<InformationFilterMetric>();
+
                 for (int j = 0; j < RuleSets[i].Rules.Length; j++)
                 {
-                    if ((RuleSets[i].Rules[j].Metric.GetCurrentValue() >= RuleSets[i].Rules[j].Value) != RuleSets[i].Rules[j].Minimum)
+                    //Check if actual value behaves to required value as necessary
+                    bool conditionMet = (RuleSets[i].Rules[j].Metric.GetCurrentValue() >= RuleSets[i].Rules[j].Value) !=
+                                        RuleSets[i].Rules[j].Minimum;
+
+                    //Add metrics to appropriate lists in case the
+                    if (conditionMet == false)
                     {
-                        conditionsMet = false;
-                        if (RuleSets[i]._previouslytrue)
-                        {
-                            //TODO: only add if not contained
-                            _changedMetrics.Add(RuleSets[i].Rules[j].Metric);
-                        }
+                        allConditionsMet = false;
+                        falseMetrics.Add(RuleSets[i].Rules[j].Metric);
+                    }
+                    else
+                    {
+                        trueMetrics.Add(RuleSets[i].Rules[j].Metric); 
                     }
                 }
-                RuleSets[i]._previouslytrue = conditionsMet;
+
+                //If the state of the ruleset changed, add the affected metrics to the list 
+                if (RuleSets[i]._previouslytrue != allConditionsMet)
+                {
+                    ExtendListWithoutOverlap(ref changedMetrics, allConditionsMet?trueMetrics:falseMetrics);
+                }
+                RuleSets[i]._previouslytrue = allConditionsMet;
             }
-            if (_changedMetrics.Count > 0)
+            //If any rulesets have changed states, call ConditionsMet in the ones now true that use the affected metrics
+            if (changedMetrics.Count > 0)
             {
-                //Go through everything and call the events in the rulesets that are previouslytrue and use the metrics in the list
+                for (int i = 0; i < RuleSets.Length; i++)
+                {
+                    if (RuleSets[i]._previouslytrue && RuleSetContainsAnyMetric(RuleSets[i], changedMetrics))
+                    {
+                        RuleSets[i].ConditionsMet.Invoke();
+                    }
+                }
             }
+        }
+
+        private void ExtendListWithoutOverlap(ref List<InformationFilterMetric> baseList, List<InformationFilterMetric> extension)
+        {
+            for (int i = extension.Count - 1; i >= 0; i--)
+            {
+                if (!baseList.Contains(extension[i]))
+                {
+                    baseList.Add(extension[i]);
+                }
+            }   
+        }
+
+        private bool RuleSetContainsAnyMetric(RuleSet ruleSet, List<InformationFilterMetric> metrics)
+        {
+            for (int i = metrics.Count - 1; i >= 0; i--)
+            {
+                for (int j = ruleSet.Rules.Length - 1; j >= 0; j--)
+                {
+                    if (metrics[i] == ruleSet.Rules[j].Metric)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 
