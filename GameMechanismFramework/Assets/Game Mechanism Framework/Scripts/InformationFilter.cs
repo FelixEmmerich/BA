8809a34b03857a47c97edd26c7f4358fad969fde
FelixEmmerich <Felix-Emmerich@Freenet.de>
2017-02-07 15:42:18 +0100

    Began work on extended InformationFilters.

---
+++ GameMechanismFramework/Assets/Game Mechanism Framework/Scripts/InformationFilter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GameMechanism
+{
+    public class InformationFilter : MonoBehaviour
+    {
+
+        [Serializable]
+        public struct Rule
+        {
+            public InformationFilterMetric Metric;
+            public float Value;
+            [Tooltip("Whether the value is a minimum or a maximum.")]
+            public bool Minimum;
+        }
+
+        [Serializable]
+        public struct RuleSet
+        {
+            public Rule[] Rules;
+            public UnityEvent ConditionsMet;
+            [HideInInspector]
+            public bool _previouslytrue;
+        }
+
+        public RuleSet[] RuleSets;
+
+        // Update is called once per frame
+        void Update()
+        {
+            CheckRuleSets();
+        }
+
+        public void CheckRuleSets()
+        {
+            List<InformationFilterMetric> _changedMetrics = new List<InformationFilterMetric>();
+            for (int i = 0; i < RuleSets.Length; i++)
+            {
+                bool conditionsMet = true;
+                for (int j = 0; j < RuleSets[i].Rules.Length; j++)
+                {
+                    if ((RuleSets[i].Rules[j].Metric.GetCurrentValue() >= RuleSets[i].Rules[j].Value) != RuleSets[i].Rules[j].Minimum)
+                    {
+                        conditionsMet = false;
+                        if (RuleSets[i]._previouslytrue)
+                        {
+                            //TODO: only add if not contained
+                            _changedMetrics.Add(RuleSets[i].Rules[j].Metric);
+                        }
+                    }
+                }
+                RuleSets[i]._previouslytrue = conditionsMet;
+            }
+            if (_changedMetrics.Count > 0)
+            {
+                //Go through everything and call the events in the rulesets that are previouslytrue and use the metrics in the list
+            }
+        }
+    }
+
+}
---
+++ GameMechanismFramework/Assets/Game Mechanism Framework/Scripts/InformationFilterMetric.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameMechanism
+{
+    public abstract class InformationFilterMetric
+    {
+        private bool _updatedFlag = false;
+        private float _currentValue;
+
+        public float GetCurrentValue()
+        {
+            if (!_updatedFlag)
+            {
+                _currentValue = GetNewestValue();
+            }
+            return _currentValue;
+        }
+
+        private void Update()
+        {
+            _updatedFlag = false;
+        }
+
+        protected abstract float GetNewestValue();
+    }
+
+}
